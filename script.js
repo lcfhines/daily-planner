86b6d2bc7304e3597c41645be71d204dd39098a9
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-06 18:27:24 -0400

    added timeblocks

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
 
+
+
 // display current day at the top of the calendar
  var currentDay = moment();
  $("#currentDay").text(currentDay.format("dddd, MMM Do"));
+
+// colorcode time blocks based on past/present/future
+// format timeblock hours
+
+var timeblock = $('.timeblock').text;
+var presentHour = moment().format("h");
+var eventblock = $('.event');
+
+if (timeblock < presentHour) {
+    eventblock.css({'background-color': 'yellow'});
+}
+
+
