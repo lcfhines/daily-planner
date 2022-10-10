e8a42b6f55b2e59be4ac0f88393855de5c920c44
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-10 19:27:55 -0400

    added updates to get save button working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
-
+var saveButton = $('.saveBtn');
+var inputEl = $('.eventInput');
 
 // display current day at the top of the calendar
  var currentDay = moment();
@@ -9,12 +9,33 @@
 // colorcode time blocks based on past/present/future
 // format timeblock hours
 
-var timeblock = $('.timeblock').text;
-var presentHour = moment().format("h");
-var eventblock = $('.event');
+// var timeblock = $('.timeblock').text;
+// var presentHour = moment().format("h");
+// var eventblock = $('.event');
 
-if (timeblock < presentHour) {
-    eventblock.css({'background-color': 'yellow'});
+// if (timeblock < presentHour) {
+//     eventblock.css({'background-color': 'yellow'});
+// }
+
+
+
+
+// submitting form
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var eventItem = $('input[name="eventInput"]').val();
+    if (!eventItem) {
+        console.log('No event entered');
+        return;
+    }
+    // need to tell it where to put text
+        storedEvent.textContent = eventItem;
+    // storedEvent.append(eventItem)
+    // store to local value
+    localStorage.setItem("stored event", eventItem);
+    $('input[name="eventInput"]').val('');
 }
 
-
+saveButton.on('click', handleFormSubmit);
