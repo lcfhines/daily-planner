4cf485e1dea954e91cba862ee1e88b5142b8e729
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-10 22:05:59 -0400

    added color coding and saving events to local storage

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
 
 var saveButton = $('.saveBtn');
-var inputEl = $('.eventInput');
 
+
+$(document).ready(function() {
 // display current day at the top of the calendar
- var currentDay = moment();
- $("#currentDay").text(currentDay.format("dddd, MMM Do"));
+    var currentDay = moment();
+    $("#currentDay").text(currentDay.format("dddd, MMM Do"));
+    
+    // assign save button click listener for user's input
+    saveButton.on("click", function() {
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+        console.log(block9);
+    })
 
-// colorcode time blocks based on past/present/future
-// format timeblock hours
+// get the data stored to local storage for each block, load into corresponding block
+$("#block9 .description").val(localStorage.getItem("block9"));
+$("#block10 .description").val(localStorage.getItem("block10"));
+$("#block11 .description").val(localStorage.getItem("block11"));
+$("#block12 .description").val(localStorage.getItem("block12"));
+$("#block1 .description").val(localStorage.getItem("block1"));
+$("#block2 .description").val(localStorage.getItem("block2"));
+$("#block3 .description").val(localStorage.getItem("block3"));
+$("#block4 .description").val(localStorage.getItem("block4"));
+$("#block5 .description").val(localStorage.getItem("block5"));
 
-// var timeblock = $('.timeblock').text;
-// var presentHour = moment().format("h");
-// var eventblock = $('.event');
+function colorCode() {
+    var presentHour = moment().hour();
+    console.log(presentHour);
+    $(".time-block").each(function() {
+        var hourBlock = parseInt($(this).attr("id").split("block")[1]);
 
-// if (timeblock < presentHour) {
-//     eventblock.css({'background-color': 'yellow'});
-// }
+        // use past present and future classes to indicate where the timeblock stands based on the current time
 
+        if (hourBlock < presentHour){
+           $(this).addClass("past");
+           $(this).removeClass("present");
+           $(this).removeClass("future");
+            } else if (hourBlock === presentHour) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            } else {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            }
 
-
-
-// submitting form
-
-function handleFormSubmit(event) {
-    event.preventDefault();
-
-    var eventItem = $('input[name="eventInput"]').val();
-    if (!eventItem) {
-        console.log('No event entered');
-        return;
-    }
-    // need to tell it where to put text
-        storedEvent.textContent = eventItem;
-    // storedEvent.append(eventItem)
-    // store to local value
-    localStorage.setItem("stored event", eventItem);
-    $('input[name="eventInput"]').val('');
+    })
 }
 
-saveButton.on('click', handleFormSubmit);
+colorCode();
+
+})
